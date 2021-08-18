3bf66fe7955afe79c17333820a27cf33950b3e84
Aleksey <1862647@gmail.com>
2021-08-18 16:54:24 +0700

    Popup edit profile

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('hello')
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__button-close");
+const popupEditProfile = document.querySelector(".popup");
+const popupInput = document.querySelectorAll(".popup__input");
+const popupButtonSave = document.querySelector(".popup__button-save");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubTitle = document.querySelector(".profile__subtitle");
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", () => {
+  openPopup(popupEditProfile);
+});
+
+closeButton.addEventListener("click", () => {
+  closePopup(popupEditProfile);
+});
+
+popupButtonSave.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  popupInput.forEach(element => {
+    let inputName = element.getAttribute("name");
+    if (inputName === "name") {
+      inputValue = element.value;
+      profileTitle.textContent = inputValue;
+    }
+    if (inputName === "description") {
+      inputValue = element.value;
+      profileSubTitle.textContent = inputValue;
+    }
+  });
+  closePopup(popupEditProfile);
+});
